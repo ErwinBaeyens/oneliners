9b560a570c20b96a423ce0cdb88155489b4bd461
Erwin Baeyens <erwin.baeyens@hgst.com>
2016-05-13 09:57:00 +0200

    rewrote the definiton of the networkdc and network update structures

--- update_scaler_nic.py
+++ update_scaler_nic.py
@@ -1,24 +1,22 @@
-from marvin_lib import ha
+from marvin_lib.network import ha
 import json
 import time
 
 
 # setup the new network data
-
-networkdc = {}
-networkdc['network'] = '<network address>'
-networkdc['netmask'] = '<network netmask>'
-networkdc['status'] = 'ENABLED'
-networkdc['name'] = 'PublicLanDC'
+networkdc = {
+    'network':'<network address>',
+    'netmask':'<network netmask>',
+    'status':'ENABLED',
+    'name':'PublicLanDC'
+}
 
 
 # create the new network
-
 print ha.post('/model/network', data=json.dumps(networkdc))
 
 
 # get the guid from the new network
-
 networks = json.loads(ha.get('/model/network'))
 for net in networks['network']:
     if net['name'] ==  networkdc['name']:
@@ -26,7 +24,6 @@ for net in networks['network']:
 
 
 # get machine data and get nic data
-
 for machine in cli.marvin.machine.list():
     print machine['name']
     for nic in machine['nic']:
@@ -34,25 +31,24 @@ for machine in cli.marvin.machine.list():
 
         
 # modify the nicUpdate object
-
-nicUpdate = {}
-nicUpdate['address'] = '<new ip address>'
-nicUpdate['netmask'] = '<new netmask>'
-nicUpdate['network'] = net_guid
+nicUpdate = {
+    'address':'<new ip address>',
+    'netmask':'<new netmask>',
+    'network': net_guid
+}
 
 
 # push the new data
-
-print ha.put('/model/machine/<host name>/nic/<nic_guid>', data = json.dumps(nicUpdate))
+print ha.put('/model/machine/<host name>/nic/<nic_guid>',
+             data = json.dumps(nicUpdate)
+)
 
 
 # reconfigure the host
-
 task = cli.marvin.machine.configure('<hostname>')
 
 
 # wait for the reconfiguration task to finish
-
 id = task['guid']
 while True:
     task = cli.marvin.task.get(id)
@@ -62,9 +58,7 @@ while True:
     else:
         break
 
-    
 # verifiy
-
 for machine in cli.marvin.machine.list():
     print machine['name']
     for nic in machine['nic']:
