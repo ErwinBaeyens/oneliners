1684bbdcc0388124a4b99c57ae801729326eddd8
Erwin Baeyens <erwin.baeyens@hgst.com>
2016-06-10 14:21:26 +0200

    example on how to list all users on a scaler system

---
+++ list_all_users.py
@@ -0,0 +1,11 @@
+# list all users that are defined on a scaler system
+
+from scalerim_api.constants import SIM_IdentityFilter
+
+t_filter = SIM_IdentityFilter(True, True)
+accounts = cli.clients.scalerim.identityPageGet('','', True, True, t_filter)
+
+for account in accounts:
+    print "\t%s\t%s\t%s"%(account.displayName, account.email, account.canonicalId)
+
+    
