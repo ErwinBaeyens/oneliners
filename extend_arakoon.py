5474074c1e5fa881630b8d2df583706f4c98f0a9
Erwin Baeyens <erwin.baeyens@hgst.com>
2016-08-03 20:26:38 +0200

    added port rename

---
+++ extend_arakoon.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import json
+
+data = []
+with open('/home/erwin/Documents/ms63/tmp/arakooncluster.json') as json_file:
+    json_data = json.load(json_file)
+
+print json_data
+ = 
